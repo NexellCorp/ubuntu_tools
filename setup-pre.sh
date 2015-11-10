38748e819c6376a14d5d7aef4933c77902d8521e
Sungwoo Park <swpark@nexell.co.kr>
2015-11-10 11:27:20 +0900

    [ADD] install for yocto

--- setup-pre.sh
+++ setup-pre.sh
@@ -6,3 +6,6 @@ sudo apt-get install -y qemu-user-static
 sudo dpkg -i ${TOP}/ubuntu-build-service/packages/live-build_3.0.5-1linaro1_all.deb
 sudo apt-get install -y gcc-arm-linux-gnueabihf
 sudo apt-get install -y gcc-aarch64-linux-gnu
+
+# for yocto
+sudo apt-get install -y gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential chrpath libsdl1.2-dev xterm nano
